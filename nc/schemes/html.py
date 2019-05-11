daed7de928711f2ec1c49b77ecd2ba24643b3de9
Tom Ritchford <tom@swirly.com>
2019-05-11 17:43:28 +0200

    Add nc/schemes/html.py

---
+++ nc/schemes/html.py
@@ -0,0 +1,20 @@
+# From https://en.wikipedia.org/wiki/Web_colors#HTML_color_names
+
+COLORS = {
+    'White': 0xFFFFFF,
+    'Silver': 0xC0C0C0,
+    'Gray': 0x808080,
+    'Black': 0x000000,
+    'Red': 0xFF0000,
+    'Maroon': 0x800000,
+    'Yellow': 0xFFFF00,
+    'Olive': 0x808000,
+    'Lime': 0x00FF00,
+    'Green': 0x008000,
+    'Aqua': 0x00FFFF,
+    'Teal': 0x008080,
+    'Blue': 0x0000FF,
+    'Navy': 0x000080,
+    'Fuchsia': 0xFF00FF,
+    'Purple': 0x800080,
+}
