b89035ccff3620622418699554d03f5d0fedd5ee
Tom Ritchford <tom@swirly.com>
2019-05-14 21:21:02 +0200

    Merge to_color.py into util.py

--- nc/util.py
+++ nc/util.py
@@ -1,6 +1,9 @@
 """Functions that are not dependent on a specific NamedColors"""
 
+import numbers
 import string
+
+_ALLOWED = set(string.ascii_letters + string.digits)
 
 
 def to_rgb(color):
@@ -24,4 +27,30 @@ def canonical_name(name):
     return ''.join(i for i in name.lower() if i in _ALLOWED)
 
 
-_ALLOWED = set(string.ascii_letters + string.digits)
+def to_color(c):
+    """Try to coerce the argument into an rgb color"""
+    if isinstance(c, numbers.Number):
+        return c, c, c
+    if not c:
+        raise ValueError('Cannot create color from empty "%s"' % c)
+    if isinstance(c, tuple):
+        return c
+    if isinstance(c, list):
+        return tuple(c)
+    if not isinstance(c, str):
+        raise ValueError('Do not understand color type %s' % c)
+
+    if ',' in c:
+        c = c.lstrip('(').rstrip(')').lstrip('[').rstrip(']')
+        return tuple(from_int(i) for i in c.split(','))
+
+    h = from_hex(c)
+    if h is not None:
+        return to_rgb(h)
+
+    try:
+        n = int(c)
+    except Exception:
+        raise ValueError('Do not understand color name %s' % c)
+
+    return n, n, n
