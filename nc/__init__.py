31c54e0dde6f6bb7a2d756bf2aa0202fc5b9eb23
Tom Ritchford <tom@swirly.com>
2020-04-13 19:15:25 +0200

    Delegate magic methods from nc to Colors

--- nc/__init__.py
+++ nc/__init__.py
@@ -22,6 +22,30 @@ class NamedColors:
             pass
         return getattr(self.COLORS, name)
 
+    def __call__(self, *args, **kwargs):
+        return self.COLORS(*args, **kwargs)
+
+    def __getitem__(self, name):
+        return self.COLORS[name]
+
+    def __contains__(self, x):
+        """Return true if this string name appears in the table canonically"""
+        return x in self.COLORS
+
+    def __len__(self):
+        return len(self.COLORS)
+
+    def __iter__(self):
+        return iter(self.COLORS)
+
+    def __eq__(self, x):
+        if isinstance(x, __class__):
+            return self.COLORS == x.COLORS
+        return self.COLORS == x
+
+    def __ne__(self, x):
+        return not (self == x)
+
 
 sys.modules[__name__] = NamedColors()
 NamedColors.__doc__ = """NamedColors"""
