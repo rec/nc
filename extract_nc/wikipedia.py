1b061e32f8746189e128d59047a3f2011e993f6b
Tom Ritchford <tom@swirly.com>
2020-04-30 15:16:14 +0200

    Extract ASCII escape from wikipedia

--- extract_nc/wikipedia.py
+++ extract_nc/wikipedia.py
@@ -3,14 +3,15 @@ import bs4
 import datetime
 import os
 import pathlib
+import re
 import requests
+import safer
 import sys
 
 HEADER = """# This file was automatically generated on {}
 # by script {}
 # from {}
-
-COLORS = {{"""
+"""
 
 FOOTER = '}'
 
@@ -28,8 +29,12 @@ SOURCES = {
     'pwg': ('PWG 5101.1 colors', 'X11_color_names&section=10'),
 }
 
-TABLE_END = '|}'
+ANSI_ESCAPE_PAGE = 'ANSI_escape_code&section=12'
 
+TABLE_BEGIN = '{| '
+TABLE_LINE = '|-\n'
+TABLE_END = '|}\n'
+SCHEME_DIR = pathlib.Path(__file__).parents[1] / 'nc' / 'schemes'
 
 SPECIAL_COLORS = {
     ('Peach', 0xFFCBA4): 'Deep peach',
@@ -37,8 +42,16 @@ SPECIAL_COLORS = {
     ('Tea rose', 0xF88379): 'Tea rose orange',
 }
 
+COLSPAN_RE = re.compile('colspan="(.)"')
 
-def wikibox(url):
+
+def print_header(title, print):
+    timestamp = datetime.datetime.now().isoformat()
+    header = HEADER.format(timestamp, sys.argv[0], title)
+    print(header)
+
+
+def wikitext(url):
     if not url.startswith(WIKI_URL):
         url = WIKI_URL + url
     if not url.endswith(EDIT_STRING):
@@ -48,16 +61,13 @@ def wikibox(url):
     return soup.find(id='wpTextbox1').text
 
 
-def colors(urls, fp, title):
-    timestamp = datetime.datetime.now().isoformat()
-    header = HEADER.format(timestamp, sys.argv[0], title)
-    print(header, file=fp)
-
+def colors(urls, title, print):
+    print_header(title, print)
+    print('COLORS = {{')
     for url in urls:
-        for line in wikibox(url).splitlines():
-            if line.strip() == TABLE_END:
+        for line in wikitext(url).splitlines():
+            if line == TABLE_END:
                 break
-
             color = extract_color.extract_color(line)
             if not color:
                 continue
@@ -74,28 +84,90 @@ def colors(urls, fp, title):
                 name = name[:-2]
             name = name.split('|')[-1].strip().replace("'", "\\'")
             name = SPECIAL_COLORS.get((name, int(hexname, 16)), name)
-            print("    '{}': 0x{},".format(name, hexname), file=fp)
+            print("    '{}': 0x{},".format(name, hexname))
 
-    print(FOOTER, file=fp)
+    print(FOOTER)
 
 
 def write_colors():
-    schemes = pathlib.Path(__file__).parents[1] / 'nc' / 'schemes'
     for name, (title, *urls) in SOURCES.items():
-        filename = (schemes / name).with_suffix('.py')
-        fp = open(filename, 'w')
+        filename = (SCHEME_DIR / name).with_suffix('.py')
         print('Creating', filename, '...', end='')
-        try:
-            with fp:
-                colors(urls, fp, title)
-            print('done')
-        except Exception:
-            try:
-                os.remove(filename)
-            except Exception:
-                print('FAILED')
-            raise
+        with safer.printer(filename, 'w') as pr:
+            colors(urls, title, pr)
+        print('done')
+
+
+def write_escapes():
+    text = wikitext(ANSI_ESCAPE_PAGE)
+
+    table = text.split(TABLE_BEGIN, maxsplit=1)[-1]
+    table = table.split(TABLE_END, maxsplit=1)[0]
+
+    _, header, *rows = table.split(TABLE_LINE)
+    assert len(rows) == 16, str(rows)
+
+    names = _read_header(header)
+    names = [n.replace('&nbsp;', ' ') for n in names]
+    assert len(names) == 11, str(names)
+
+    name_to_values = {}
+    for row in rows:
+        color, values = _read_row(row)
+        if not any(v is None for v in values):
+            for name, value in zip(names, values):
+                name_to_values.setdefault(name, {})[color] = value
+
+    with safer.printer(SCHEME_DIR / 'escapes.py') as print:
+        print_header('ASCII escape codes', print)
+        first = True
+        for name, values in name_to_values.items():
+            if first:
+                first = False
+            else:
+                print()
+
+            name = ''.join(i if i.isalnum() else '_' for i in name)
+            print(name.replace('__', '_').upper(), '= (')
+            for color, value in values.items():
+                print("    ('%s', %s)," % (color, value))
+            print(')')
+
+
+def _read_row(row):
+    color = None
+    items = []
+    for row in row.splitlines():
+        parts = [i.strip() for i in row.split('|') if i.strip()]
+        if not color:
+            color = parts[0]
+        elif not parts:
+            items.append(None)
+        else:
+            style, value, *_ = parts
+            rgb = value.split('<')[0].split(',')
+            rgb = tuple(int(i) for i in rgb)
+            m = COLSPAN_RE.search(style)
+            span = m and int(m.group(1)) or 1
+            items += [rgb] * span
+
+    assert len(items) == 11, '%s=%s' % (color, items)
+    return color, items
+
+
+def _read_header(header):
+    for line in header.splitlines():
+        if line.startswith('!'):
+            line = line[1:].strip()
+            if line.startswith('['):
+                link = line.split(']')[0].strip('[]')
+                yield link.split('|')[-1]
+            elif '||' not in line:
+                yield line.split('<')[0]
 
 
 if __name__ == '__main__':
-    write_colors()
+    if False:
+        write_colors()
+    else:
+        print(write_escapes())
