45ff60c0b19a615e3a5cb44c8a393d750cd6f1e5
Tom Ritchford <tom@swirly.com>
2020-05-01 21:31:17 +0200

    Automatically extract terminal256 colors

---
+++ extract_nc/terminal_colors.py
@@ -0,0 +1,53 @@
+"""
+https://serverfault.com/a/91873
+"""
+
+import nc
+import safer
+from . import wikipedia
+from nc.schemes.terminal16 import xterm
+
+FILE = wikipedia.SCHEME_DIR / '_terminal256.py'
+
+COLORS = nc.Colors('juce', 'x11', 'html', 'pwg')
+old_print = print
+
+
+def write_256():
+    with safer.printer(FILE) as print:
+        wikipedia.print_header('an algorithm', print)
+        print('COLORS = (')
+        count = 0
+
+        cube = range(0, 256, 51)  # (0, 95, 135, 175, 215, 255)
+        colors_seen = set(xterm.COLORS)
+
+        def add_name(r, g, b):
+            color = COLORS.closest((r, g, b))
+            name = str(color)
+            index = 0
+            while name in colors_seen:
+                name = '%s %s' % (color, chr(index + ord('B')))
+                index += 1
+            colors_seen.add(name)
+            print(f"    ('{name}', ({r}, {g}, {b})),")
+            nonlocal count
+            count += 1
+
+        for r in cube:
+            for g in cube:
+                for b in cube:
+                    add_name(r, g, b)
+
+        for i in range(12, 248, 10):
+            add_name(i, i, i)
+
+        print(')')
+        print()
+        print('PRESERVE_CAPITALIZATION = True')
+
+    old_print('count =', count)
+
+
+if __name__ == '__main__':
+    write_256()
--- test/nc/scheme_test.py
+++ test/nc/scheme_test.py
@@ -11,14 +11,14 @@ X11 = Colors('x11')
 
 class SchemeTest(unittest.TestCase):
     def test_terminal256(self):
-        assert len(TERMINAL_256) == 202
+        assert len(TERMINAL_256) == 256
 
         s256 = set(TERMINAL_256.items())
-        assert len(s256.intersection(X11.items())) == 11
-        assert len(s256.intersection(HTML.items())) == 13
-        assert len(s256.intersection(WIKIPEDIA.items())) == 10
-        assert len(s256.intersection(TERMINAL_8.items())) == 5
-        assert len(s256.intersection(TERMINAL_16.items())) == 2
+        assert len(s256.intersection(X11.items())) == 4
+        assert len(s256.intersection(HTML.items())) == 3
+        assert len(s256.intersection(WIKIPEDIA.items())) == 2
+        assert len(s256.intersection(TERMINAL_8.items())) == 1
+        assert len(s256.intersection(TERMINAL_16.items())) == 1
 
     def test_terminal16(self):
         assert len(TERMINAL_16) == 16
@@ -28,4 +28,4 @@ class SchemeTest(unittest.TestCase):
         assert len(s16.intersection(HTML.items())) == 1
         assert len(s16.intersection(WIKIPEDIA.items())) == 1
         assert len(s16.intersection(TERMINAL_8.items())) == 1
-        assert len(s16.intersection(TERMINAL_256.items())) == 2
+        assert len(s16.intersection(TERMINAL_256.items())) == 1
